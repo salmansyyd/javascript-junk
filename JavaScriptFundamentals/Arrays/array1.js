2d2c23a5e0f92ff42863e188afdf65f72146639d
Sayyed-Salman <sayyedsalman2000@gmail.com>
2023-01-08 23:00:07 +0530

    arrays modified

--- JavaScriptFundamentals/Arrays/array1.js
+++ JavaScriptFundamentals/Arrays/array1.js
@@ -1,74 +1,32 @@
-console.log("Arrays 1")
+function* get_more_number(num){
 
-// Arrays are list of items with numeric indexes as identifiers 
+    while(num<10){
+        yield num;
+        num++
+    }
 
-// Creating an array
+}
 
-// 1
-let programmingLanguages = ["python", "java", "C#", "C++", "javascript"]
 
-/*
-        1'st Method note:
-    both of the methods can be interchangeable used 
-    we can also write it as 
-    and then add items with index as shown below
-    let programmingLanguages = []
 
-        2nd Method note:
-    you can directly add the items in the constructor "Array("React","Angular","Next.js")"
+function do_someting_else(){
+    console.log("Other async task")
+}
+
+// Creating a usefull generator function for implemntation of yeilding 
+
+
+const caller_obj = get_more_number(1)
+/* 
+create a generator 
+call the function once 
+do somethingg else
+call the fucntion again
+repeat until generator exhaust out of values 
 */
 
-// 2 
-let javascriptFrameworks = new Array(3)
-javascriptFrameworks[0] = "React"
-javascriptFrameworks[1] = "Angular"
-javascriptFrameworks[2] = "Next.js"
-
-// Accessing Array Elements 
-/*
-    Array methods are acessed by indexs "arrayName[index]"
-*/
-console.log(javascriptFrameworks[1]+" "+programmingLanguages[4])
-console.log(typeof javascriptFrameworks)
-console.log(`javascriptFramework is an array:${Array.isArray(javascriptFrameworks)}`)
-console.log(`another method is using instanceof,${javascriptFrameworks instanceof Array}`);
-
-
-// Array methods 
-
-/* Converting Array to strings */
-let output = document.getElementById('outputArray');
-
-output.innerHTML = javascriptFrameworks.toString();
-
-let output2 = document.getElementById('outputArray2');
-
-output2.innerHTML = programmingLanguages.join(" ")
-
-/* Push and Pop */
-let games = ["WWE2k16","NBA","Resident Evil VII","Fifa"]
-console.log(games)
-console.log(games.pop());
-console.log(games)
-
-let socialMediaPlatforms = ["Reddit","Instagram","Twitter","Snapchat","9Gag"]
-console.log(socialMediaPlatforms.push("Whatsapp"))
-console.log(socialMediaPlatforms)
-
-// Shifting using .shift() method 
-/* shift removes the first element and shifts all the other elements to a lower index */
-console.log(socialMediaPlatforms.shift())
-socialMediaPlatforms.unshift("Tinder")
-socialMediaPlatforms.push("Discord")
-console.log(socialMediaPlatforms)
-
-
-// slice() and splice()
-let fruits = ["mango","banana","orange","pineapple"]
-console.log(fruits);
-let removed = fruits.splice(0,0,"spliced")
-console.log(fruits);
-
-// sort() and reverse()
-console.log(fruits.reverse())
-console.log(fruits.sort())
+while (!caller_obj.next().done){
+    console.log(caller_obj.next().value)
+    do_someting_else()
+    console.log(caller_obj.next().value)
+}
